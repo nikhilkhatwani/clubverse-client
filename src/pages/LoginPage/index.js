8a8df75b314b5fcabda1813792533e002fe6103b
nikhilkhatwani <nikkhatwani@gmail.com>
2023-01-18 21:43:51 -0600

    idk

--- src/pages/LoginPage/index.js
+++ src/pages/LoginPage/index.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
+import { Loading } from "../../components";
 import { handleKeyDown } from "../../utils/api";
 import { getSchools } from "../../utils/api/calls/schools";
 import { userLogin } from "../../utils/api/calls/users";
@@ -69,11 +70,22 @@ export default function LoginPage({ setToken, setUser }) {
         setSchoolNames(schoolNames);
         setSchools(response.schools);
         setLoading(false);
+      } else {
+        setError(response.message);
+        setLoading(false);
       }
     }
 
     getAllSchools();
   }, []);
+
+  const goToBottomOfForm = () => {
+    console.log("scrolling");
+
+    const form = document.querySelector(".login-form");
+    console.log(form);
+    form.scrollTo({ top: form.scrollHeight, behavior: "smooth" });
+  };
 
   return (
     <div className="login-body">
@@ -135,6 +147,22 @@ export default function LoginPage({ setToken, setUser }) {
           </button>
         </div>
       </div>
+
+      <button onClick={goToBottomOfForm} className="form-go-down-button">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          class="bi bi-chevron-down"
+          viewBox="0 0 16 16"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"
+          />
+        </svg>
+      </button>
     </div>
   );
 }
